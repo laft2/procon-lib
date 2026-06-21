0e037eb2ae0d066f74a04ac9f6719486d44e2999
laft2 <44256206+laft2@users.noreply.github.com>
2026-06-21 13:00:32 +0900

    rename: mod_int -> mint

--- math/modint.hpp
+++ math/modint.hpp
@@ -4,32 +4,32 @@
 using ll = long long;
 using namespace std;
 
-template <ll Mod> struct mod_int {
+template <ll Mod> struct modint {
   static const ll mod = Mod;
   ll val;
-  mod_int(ll sig = 0) {
+  modint(ll sig = 0) {
     sig %= mod;
     if (sig < 0)
       sig += mod;
     val = sig;
   }
 
-  constexpr mod_int operator-() const { return val ? mod - val : 0; }
-  constexpr mod_int &operator+=(const mod_int &that) {
+  constexpr modint operator-() const { return val ? mod - val : 0; }
+  constexpr modint &operator+=(const modint &that) {
     if ((val += that.val) >= mod)
       val -= mod;
     return *this;
   }
-  constexpr mod_int &operator-=(const mod_int &that) {
+  constexpr modint &operator-=(const modint &that) {
     if ((val += mod - that.val) >= mod)
       val -= mod;
     return *this;
   }
-  constexpr mod_int &operator*=(const mod_int &that) {
+  constexpr modint &operator*=(const modint &that) {
     val = (unsigned long long)val * that.val % mod;
     return *this;
   }
-  constexpr mod_int &operator/=(const mod_int &r) noexcept {
+  constexpr modint &operator/=(const modint &r) noexcept {
     ll a = r.val, b = Mod, u = 1, v = 0;
     while (b) {
       ll t = a / b;
@@ -43,40 +43,40 @@ template <ll Mod> struct mod_int {
       val += Mod;
     return *this;
   }
-  constexpr mod_int operator+(const mod_int &that) const {
-    return mod_int(*this) += that;
+  constexpr modint operator+(const modint &that) const {
+    return modint(*this) += that;
   }
-  constexpr mod_int operator-(const mod_int &that) const {
-    return mod_int(*this) -= that;
+  constexpr modint operator-(const modint &that) const {
+    return modint(*this) -= that;
   }
-  constexpr mod_int operator*(const mod_int &that) const {
-    return mod_int(*this) *= that;
+  constexpr modint operator*(const modint &that) const {
+    return modint(*this) *= that;
   }
-  constexpr mod_int operator/(const mod_int &that) const {
-    return mod_int(*this) /= that;
+  constexpr modint operator/(const modint &that) const {
+    return modint(*this) /= that;
   }
-  constexpr bool operator==(const mod_int &that) const noexcept {
+  constexpr bool operator==(const modint &that) const noexcept {
     return this->val == that.val;
   }
-  constexpr bool operator!=(const mod_int &that) const noexcept {
+  constexpr bool operator!=(const modint &that) const noexcept {
     return this->val != that.val;
   }
-  friend constexpr ostream &operator<<(ostream &os, const mod_int &x) noexcept {
+  friend constexpr ostream &operator<<(ostream &os, const modint &x) noexcept {
     return os << x.val;
   }
-  friend constexpr istream &operator>>(istream &is, mod_int &x) noexcept {
+  friend constexpr istream &operator>>(istream &is, modint &x) noexcept {
     return is >> x.val;
   }
 
-  friend mod_int power(const mod_int &a, long long n) noexcept {
-    mod_int res = 1, r = a;
+  friend modint power(const modint &a, long long n) noexcept {
+    modint res = 1, r = a;
     for (; n > 0; n >>= 1, r *= r)
       res *= (n & 1 ? r : 1);
     return res;
   }
   static int sz;
-  static vector<mod_int> fact, infact;
+  static vector<modint> fact, infact;
 };
 
-using modint1000000007 = mod_int<1000000007>;
-using modint998244353 = mod_int<998244353>;
+using modint1000000007 = modint<1000000007>;
+using modint998244353 = modint<998244353>;
